cc283c1e52c5cc714ceffe8c3a662535165d6738
Christopher <christopher@mogler.dev>
2024-05-24 11:34:02 +0200

    concept

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,125 +1,214 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:moment_dart/moment_dart.dart';
 
 void main() {
+  Moment.setGlobalLocalization(MomentLocalizations.de());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    List<InventoryItem> items = [
+      InventoryItem(
+          name: "Käse",
+          expire: Moment(DateTime.now()).add(const Duration(days: -365)).date),
+      InventoryItem(
+          name: "Frischmilch",
+          expire: Moment(DateTime.now()).add(const Duration(days: -2)).date),
+      InventoryItem(
+          name: "Frischmilch",
+          expire: Moment(DateTime.now()).add(const Duration(days: 2)).date),
+      InventoryItem(
+          name: "Eier",
+          expire: Moment(DateTime.now()).add(const Duration(days: 2)).date),
+      InventoryItem(
+          name: "Joghurt",
+          expire: Moment(DateTime.now()).add(const Duration(days: 6)).date),
+      InventoryItem(
+          name: "Pizza",
+          expire: Moment(DateTime.now()).add(const Duration(days: 8)).date),
+      InventoryItem(
+          name: "Hackfleisch",
+          expire: Moment(DateTime.now()).add(const Duration(days: 32)).date),
+      InventoryItem(
+          name: "Gemüse",
+          expire: Moment(DateTime.now()).add(const Duration(days: 32)).date),
+      InventoryItem(
+          name: "Apfel Saft",
+          expire: Moment(DateTime.now()).add(const Duration(days: 365)).date),
+    ];
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+          backgroundColor:
+              const Color(0xFF6C63FF), // Set the app bar color here
+        ),
+        body: Container(
+          color: const Color(0xFFD2BBEA), // Set the background color here
+          child: InventoryListWidget(items: items),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+// Models
+// #####################################################################################################
+
+@immutable
+class InventoryItem {
+  final String name;
+  final DateTime expire;
+
+  get isExpired => expire.isBefore(DateTime.now());
+  get fromNow => expire.toMoment().fromNow();
+
+  const InventoryItem({required this.name, required this.expire});
+}
+
+@immutable
+class InventoryItemWithIcon extends InventoryItem {
+  final FaIcon icon;
+
+  const InventoryItemWithIcon(
+      {required super.name, required super.expire, required this.icon});
+}
+
+@immutable
+class InventoryItemWithImage extends InventoryItem {
+  final String imageUrl;
+
+  const InventoryItemWithImage(
+      {required super.name, required super.expire, required this.imageUrl});
+}
+
+// Widgets
+// #####################################################################################################
+
+class CheckBoxWidget extends StatefulWidget {
+  final bool isChecked;
+  final void Function(bool?) onChanged;
+
+  const CheckBoxWidget(
+      {super.key, required this.isChecked, required this.onChanged});
+
+  @override
+  State<CheckBoxWidget> createState() => _CheckBoxWidgetState();
+}
+
+class _CheckBoxWidgetState extends State<CheckBoxWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(value: widget.isChecked, onChanged: widget.onChanged);
+  }
+}
+
+class InventoryInheritedWidget extends InheritedWidget {
+  final List<InventoryItem> items;
+
+  const InventoryInheritedWidget(
+      {super.key, required this.items, required super.child});
+
+  static InventoryInheritedWidget? of(BuildContext context) {
+    return context
+        .dependOnInheritedWidgetOfExactType<InventoryInheritedWidget>();
+  }
+
+  updateItem(int index, InventoryItem item) {
+    items[index] = item;
+  }
+
+  @override
+  bool updateShouldNotify(covariant InventoryInheritedWidget oldWidget) {
+    return items != oldWidget.items;
+  }
+}
+
+class InventoryItemWidget extends StatelessWidget {
+  final InventoryItem item;
+
+  const InventoryItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    // when still good 5603AD
+    // when expired AD0392
+    // Text Color FFFFFF
+
+    return Container(
+      // height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color:
+            item.isExpired ? const Color(0xFFAD0392) : const Color(0xFF5603AD),
+      ),
+      padding: const EdgeInsets.all(10),
+      // color: item.isExpired ? Color(0xFFAD0392) : Color(0xFF5603AD),
+      child: Row(children: [
+        Container(
+          margin: const EdgeInsets.only(right: 10),
+          child: const FaIcon(
+            FontAwesomeIcons.cheese,
+            color: Color(0xFFFFFFFF),
+          ),
+        ),
+        Expanded(
+            child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              item.name,
+              style: const TextStyle(
+                  color: Color(0xFFFFFFFF),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              item.fromNow,
+              style: const TextStyle(color: Color(0xFFFFFFFF)),
+            ),
+          ],
+        )),
+        Column(
+          children: [
+            CheckBoxWidget(
+                isChecked: false,
+                onChanged: (bool? value) {
+                  print("Checkbox changed to $value");
+                }),
+          ],
+        )
+      ]),
+    );
+  }
+}
+
+class InventoryListWidget extends StatelessWidget {
+  final List<InventoryItem> items;
+  const InventoryListWidget({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    return InventoryInheritedWidget(
+        items: items,
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return Container(
+                  margin: const EdgeInsets.only(bottom: 10),
+                  child: InventoryItemWidget(item: item),
+                );
+              }),
+        ));
   }
 }
