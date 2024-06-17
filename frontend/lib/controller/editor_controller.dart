8499eb00948f8b213a43830b13eb7a1d61f44cd4
Christopher <christopher@mogler.dev>
2024-06-17 23:11:30 +0200

    removed unused code

--- frontend/lib/controller/editor_controller.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shelf_guardian/product/models/product_model.dart';
-
-class EditorViewModel {}
-
-class EditorController extends Cubit<EditorViewModel> {
-  EditorController(Product product) : super(EditorViewModel());
-}
