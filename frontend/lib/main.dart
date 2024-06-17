537c01b52df3c30a165b0e586d5afa7148184b16
Christopher Jaeger <christopher@mogler.dev>
2024-06-17 13:42:23 +0200

    implemented supabase

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,9 +4,17 @@ import 'package:moment_dart/moment_dart.dart';
 import 'package:shelf_guardian/controller/settings_controller.dart';
 import 'package:shelf_guardian/controller/user_controller.dart';
 import 'package:shelf_guardian/routes.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Moment.setGlobalLocalization(MomentLocalizations.de());
+
+  await Supabase.initialize(
+    url: 'https://kong.mogler.dev/',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzE4NTc1MjAwLAogICJleHAiOiAxODc2MzQxNjAwCn0.eTDeiCu4cGlRez4RXd9UUZZu-6OQ22i88EEAnI7PsCA',
+  );
   runApp(const ShelfGuardianApp());
 }
 
