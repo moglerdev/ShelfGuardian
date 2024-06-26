fdc88cc384e36e1e84b71676284426f764f7e982
jona.boecker <jona.boecker@web.de>
2024-06-26 14:19:39 +0200

    implemented user mail display and product count in settings page

--- lib/settings/views/settings_view.dart
+++ lib/settings/views/settings_view.dart
@@ -6,23 +6,30 @@ import 'package:shelf_guardian/auth/bloc/auth_controller.dart';
 import 'package:shelf_guardian/common/routes_service.dart';
 import 'package:shelf_guardian/components/input_field.dart';
 import 'package:shelf_guardian/product/bloc/product_controller.dart';
-import 'package:shelf_guardian/product/bloc/product_state.dart';
 import 'package:shelf_guardian/settings/components/settings_item_checkbox.dart';
 import 'package:shelf_guardian/settings/components/settings_item_descriptional.dart';
 import 'package:shelf_guardian/settings/bloc/settings_controller.dart';
 
+import 'package:shelf_guardian/product/bloc/product_state.dart';
+
 class SettingsPageView extends StatelessWidget {
   const SettingsPageView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<SettingsControllerCubit, SettingsState>(
         builder: (context, state) {
-      var productCntrl = context.read<ProductControllerCubit>();
-      var len = productCntrl.state.getProducts().length;
+      var productController = context.watch<ProductControllerCubit>();
+      var productAmmount = productController.state.getProducts().length;
+      if (productController.state is ProductListLoading) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
       return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
         InputField(
             name: "User",
-            value: "Tobi",
+            value: context.watch<AuthControllerCubit>().getUserEmail(),
             icon: FontAwesomeIcons.arrowRightFromBracket,
             onIconTap: () {
               context.read<AuthControllerCubit>().signOut();
@@ -33,12 +40,12 @@ class SettingsPageView extends StatelessWidget {
             name: "Produkte",
             description: "Hier sehen sie die Anzahl "
                 "der Produkte in ihrem Inventar",
-            value: "$len"),
+            value: "$productAmmount"),
         const SettingsItemDescriptional(
             name: "Inventar Wert",
             description:
                 "Hier wird der gesamte Warenwert ihres Inventars angegeben",
-            value: "200,00€"),
+            value: "XXX,XX€"),
         SettingsItemCheckbox(
           name: "Benachrichtigung",
           description: "Wollen sie von uns Benachrichtigungen erhalten?"
