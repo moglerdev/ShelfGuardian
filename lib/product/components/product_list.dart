f1abbd8adb26d35b3083e3e836afac398716f06c
TobiasReyEye <tobi-stoehr@t-online.de>
2024-06-27 12:18:43 +0200

    Product is now refreshable

--- lib/product/components/product_list.dart
+++ lib/product/components/product_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shelf_guardian/product/components/product_item.dart';
 import 'package:shelf_guardian/product/models/product_model.dart';
+import 'package:shelf_guardian/product/bloc/product_controller.dart';
 
 class ProductList extends StatelessWidget {
   final List<Product> products;
@@ -18,28 +20,31 @@ class ProductList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.only(left: 10, right: 10),
-        child: ListView.builder(
-            itemCount: products.length + 1,
-            itemBuilder: (context, index) {
-              if (index == products.length) {
-                return const SizedBox(height: 100);
-              }
-              final product = products[index];
-              final isSelected = selectedProducts.contains(product);
-              return Container(
-                  margin: const EdgeInsets.only(bottom: 10),
-                  child: ProductItem(
-                      product: product,
-                      isSelected: isSelected,
-                      onTap: () {
-                        if (selectedProducts.isNotEmpty) {
-                          onSelectChanged(product, !isSelected);
-                          return;
-                        }
-                      },
-                      onSelectChanged: (selected) {
-                        onSelectChanged(product, selected);
-                      }));
-            }));
+        child: RefreshIndicator(
+          onRefresh: context.read<ProductControllerCubit>().initProducts,
+          child: ListView.builder(
+              itemCount: products.length + 1,
+              itemBuilder: (context, index) {
+                if (index == products.length) {
+                  return const SizedBox(height: 100);
+                }
+                final product = products[index];
+                final isSelected = selectedProducts.contains(product);
+                return Container(
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: ProductItem(
+                        product: product,
+                        isSelected: isSelected,
+                        onTap: () {
+                          if (selectedProducts.isNotEmpty) {
+                            onSelectChanged(product, !isSelected);
+                            return;
+                          }
+                        },
+                        onSelectChanged: (selected) {
+                          onSelectChanged(product, selected);
+                        }));
+              }),
+        ));
   }
 }
