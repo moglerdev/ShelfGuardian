e4a0311fef31fd43c6b1095d53c2fae468514905
TobiasReyEye <tobi-stoehr@t-online.de>
2024-06-27 13:21:53 +0200

    Started to implement Searching

--- lib/product/views/product_view.dart
+++ lib/product/views/product_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shelf_guardian/product/bloc/product_state.dart';
 import 'package:shelf_guardian/product/components/product_list.dart';
 import 'package:shelf_guardian/product/bloc/product_controller.dart';
+import 'package:shelf_guardian/components/input_field.dart';
 
 class ProductPageView extends StatelessWidget {
   const ProductPageView({super.key});
@@ -32,6 +34,25 @@ class ProductPageView extends StatelessWidget {
               }
             },
             selectedProducts: selectedProducts);
+      } else if (state is ProductSearchedList) {
+        final products = state.products;
+        final search = state.search;
+        return Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          InputField(
+              name: "Suche",
+              value: search,
+              icon: FontAwesomeIcons.squareXmark,
+              onIconTap: () {}),
+          Expanded(
+              child: ProductList(
+                  products: products,
+                  onSelectChanged: (product, selected) {
+                    context
+                        .read<ProductControllerCubit>()
+                        .selectProduct(product);
+                  },
+                  selectedProducts: state.products))
+        ]);
       } else if (state is ProductListEmpty) {
         return const Center(
           child: Text('No products found'),
