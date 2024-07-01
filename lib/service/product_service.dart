cb41fb9034767ea071f95cb03852498f592beb7c
Christopher <christopher@mogler.dev>
2024-07-02 01:21:09 +0200

    implemented realtime channel for products

--- lib/service/product_service.dart
+++ lib/service/product_service.dart
@@ -21,6 +21,7 @@ abstract class ProductService {
 
 class ProductServiceSupabase implements ProductService {
   final client = Api.client;
+  final channel = Api.client.channel("products_items");
 
   @override
   Future<List<Product>> getProducts() async {
