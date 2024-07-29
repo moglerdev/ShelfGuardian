ab6a61578b2a9932902e7efdfea0395a3f613493
Christopher Jaeger <christopher@mogler.dev>
2024-07-29 22:23:31 +0200

    removed unused controller

--- lib/features/scanner/bloc/scanner_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-abstract class ScannerState {}
-
-class ScannerScanning extends ScannerState {}
-
-class ScannerControllerCubit extends Cubit<ScannerState> {
-  ScannerControllerCubit() : super(ScannerScanning());
-}
